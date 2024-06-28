b81c3c2291cac978a8a09d9e7b9b45bab06df39a
Elias <jmenafonseca28@gmail.com>
2024-06-28 01:33:21 -0600

    tratando de compilar

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'M3U_IONIC',
+  appName: 'mim3uapp',
   webDir: 'www'
 };
 
